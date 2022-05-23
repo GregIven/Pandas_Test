5edd382729fdbf43432d5f1c600349dd0837aa8f
GregIven <gdrew21@gmail.com>
2022-05-23 16:24:16 -0700

    file works well, I wonder if I could find a more modular file path method instead of them being so static

--- Convert.py
+++ Convert.py
@@ -1,15 +1,18 @@
 from pathlib import Path
 import os
+import re
 import pandas as pd
 
 base_file_path = Path(r'C:\Users\g.drew\OneDrive - Medivant Health\Documents\Python_projects\source_files\outscraperMedSpa1.csv')
 source_dir_path = Path(r'C:\Users\g.drew\Medivant Health\Medivant IT - Documents\Ketamine Customer List')
+destination_path = Path(r'C:\Users\g.drew\OneDrive - Medivant Health\Documents\Python_projects\source_files\Finished_formatting')
 read_file2 = pd.read_csv(base_file_path, encoding="ISO-8859-1", engine="python")
-
 
 def DropCols(filepath):
     source_file_path = Path(filepath)
-    print(source_file_path)
+    file_path_split = re.split((r'[_]'), source_file_path.name)[1]
+    print(file_path_split)
+    file_path_split = file_path_split + '.xlsx'
     try: 
         pandas_file = pd.read_excel(source_file_path)
     except OSError as err:
@@ -22,24 +25,11 @@ def DropCols(filepath):
 
     file_with_dropped = pandas_file.drop(arr_to_drop, axis=1)
 
-    if (file_with_dropped.size > 0):
-        print('dropped! {}'.format(file_with_dropped.head(1)))
+    final_path = destination_path.joinpath(file_path_split)
+
+    file_with_dropped.to_excel(final_path, index=False)
 
 with os.scandir(source_dir_path) as ketfolder:
     for file in ketfolder:
-        item = next(ketfolder)
-        DropCols(item.path)
-
-
-
-# print(arr_to_drop)
-
-
-# print(read_file.head())
-# print(read_file.index)
-# print(read_file.columns)    
-
-# print(read_file2.columns)
-
-# print(file_with_dropped.columns)
-# file_with_dropped.to_csv(r'C:\Users\g.drew\OneDrive - Medivant Health\Documents\Python_projects\source_files\Finished_formatting\')
+        if file.is_file:
+            DropCols(file.path)
